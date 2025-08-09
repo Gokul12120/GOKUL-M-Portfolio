967281d100d917e6f1b672446279056f23643360
Gokul12120 <162404513+Gokul12120@users.noreply.github.com>
2025-08-09 16:01:54 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
-const nextConfig = { reactStrictMode: true }; module.exports = nextConfig;
-// next.config.js
 const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  output: 'export', // enables static export
+const nextConfig = {
+  output: 'export',
   basePath: isProd ? '/GOKUL-M-Portfolio' : '',
   assetPrefix: isProd ? '/GOKUL-M-Portfolio/' : '',
+  images: { unoptimized: true },
 };
+
+module.exports = nextConfig;
